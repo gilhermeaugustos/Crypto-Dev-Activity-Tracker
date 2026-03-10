e1b5f14b395d54e1ac1258ff5e8f1baf2c9c321a
gilhermeaugustos <gilhermeaugustos@gmail.com>
2026-03-10 16:49:26 +0700

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,23 @@
+const PROJECTS=[
+
+{
+name:"Uniswap",
+repo:"Uniswap/v3-core"
+},
+
+{
+name:"Arbitrum",
+repo:"OffchainLabs/nitro"
+},
+
+{
+name:"Starknet",
+repo:"starkware-libs/starknet"
+},
+
+{
+name:"ZKSync",
+repo:"matter-labs/zksync-era"
+}
+
+]
